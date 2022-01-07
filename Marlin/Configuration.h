3709170a3aa60fb4fd5c9817ddf5a2bb2ab453a7
Ryan Blue <ryanzblue@gmail.com>
2022-01-06 21:48:09 -0500

    Fix bed limits with new hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1403,11 +1403,11 @@
 #define Y_BED_SIZE 230
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -3
+#define Y_MIN_POS -7
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 25 // Extended max to allow the probe to reach more of the bed.
-#define Y_MAX_POS Y_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE - 7
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
